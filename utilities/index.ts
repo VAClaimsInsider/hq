9f05e7c00f65c7942bff12f2c839fcbca7e31c45
Lee Wise <theleewise@users.noreply.github.com>
2023-03-27 16:55:56 -0500

    IDK

--- utilities/index.ts
+++ utilities/index.ts
@@ -1,4 +1,6 @@
-export const formatDate = (date: Date | string) => {
+export const formatDate = (date?: Date | string) => {
+  if (!date) return '';
+  
   const dateObj = (date instanceof Date) ? date : new Date(date);
   return new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })
     .format(dateObj);
